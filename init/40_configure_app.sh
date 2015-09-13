bdfa2aa3fac21245ee5599260e0d717c7f79fbd2
sparklyballs <sparklyballs@gmail.com>
2015-09-13 11:14:55 +0100

    change if block in init for testing db as initialise key

--- init/40_configure_app.sh
+++ init/40_configure_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ ! -f "/config/etc/as.conf" ]; then
-printf 'yes\nyes\n1\n943\n9443\nyes\nyes\nno\nyes\nno\nabc\n\n' | /config/bin/ovpn-init
+if [[ $(find /config/etc/db -type f | wc -l) -eq 0 ]]; then
+printf 'DELETE\nyes\nyes\n1\n943\n9443\nyes\nyes\nno\nyes\nno\nabc\n\n' | /config/bin/ovpn-init
 /etc/init.d/openvpnas stop
 chown -R abc:abc /config
 fi
