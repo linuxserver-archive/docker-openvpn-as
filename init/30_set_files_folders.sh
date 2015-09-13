323a5d0154a2c6ccd6e1e961bc1e93f4abb2fb0a
sparklyballs <sparklyballs@gmail.com>
2015-09-13 11:12:39 +0100

    test using contents of db as initialise key

--- init/30_set_files_folders.sh
+++ init/30_set_files_folders.sh
@@ -3,7 +3,7 @@ mkdir -p /config/pid /config/log
 
 if [ ! -f "/config/bin/ovpn-init" ]; then
 cp -pr /usr/local/openvpn_as/* /config/
-rm /config/etc/as.conf
+rm -rf /config/etc/db/*
 find /config/scripts -type f -print0 | xargs -0 sed -i 's#/usr/local/openvpn_as#/config#g'
 find /config/bin -type f -print0 | xargs -0 sed -i 's#/usr/local/openvpn_as#/config#g'
 sed -i 's#=openvpn_as#=abc#g' /config/etc/as_templ.conf
