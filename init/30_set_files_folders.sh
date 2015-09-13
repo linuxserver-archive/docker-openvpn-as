b7e80523b463c80a4c2fbcec8982a7095dcaf2ab
sparklyballs <sparklyballs@gmail.com>
2015-09-13 11:18:58 +0100

    edit init for test moving pid internal to container

--- init/30_set_files_folders.sh
+++ init/30_set_files_folders.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p /config/pid /config/log
+mkdir -p /openvpn-pid /config/log
 
 if [ ! -f "/config/bin/ovpn-init" ]; then
 cp -pr /usr/local/openvpn_as/* /config/
@@ -9,8 +9,9 @@ find /config/bin -type f -print0 | xargs -0 sed -i 's#/usr/local/openvpn_as#/con
 sed -i 's#=openvpn_as#=abc#g' /config/etc/as_templ.conf
 fi
 
-if [ -f "/config/pid/openvpn.pid" ]; then
-rm /config/pid/openvpn.pid
+if [ -f "/openvpn-pid/openvpn.pid" ]; then
+rm /openvpn-pid/openvpn.pid
 fi
 
-chown -R abc:abc /config/pid /config/log
+chown -R abc:abc /config/log
+chmod -R 755 /openvpn-pid
