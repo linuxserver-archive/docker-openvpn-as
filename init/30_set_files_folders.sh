436973b12dc3171cb9e93db1f4dca44bd8c9ab7e
sparklyballs <sparklyballs@gmail.com>
2015-09-13 11:50:35 +0100

    move sock and tmp internal to container

--- init/30_set_files_folders.sh
+++ init/30_set_files_folders.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-mkdir -p /openvpn-pid /config/log
+mkdir -p /openvpn/pid /openvpn/sock /openvpn/tmp  /config/log
 
 if [ ! -f "/config/bin/ovpn-init" ]; then
 cp -pr /usr/local/openvpn_as/* /config/
-rm -rf /config/etc/db/*
+rm -rf /config/etc/db/* /config/tmp /config/sock
 find /config/scripts -type f -print0 | xargs -0 sed -i 's#/usr/local/openvpn_as#/config#g'
 find /config/bin -type f -print0 | xargs -0 sed -i 's#/usr/local/openvpn_as#/config#g'
-sed -i 's#=openvpn_as#=abc#g' /config/etc/as_templ.conf
+sed -i 's#~/tmp#/openvpn/tmp#g' /config/etc/as_templ.conf
+sed -i 's#~/sock#/openvpn/sock#g' /config/etc/as_templ.conf
 fi
 
-if [[ $(find /config/etc/sock -type f | wc -l) -ne 0 ]]; then
-rm -rf /config/etc/sock/*
+if [[ $(find /openvpn/sock -type f | wc -l) -ne 0 ]]; then
+rm -rf /openvpn/sock/*
 fi
 
-if [[ $(find /openvpn-pid -type f | wc -l) -ne 0 ]]; then
-rm -rf /openvpn-pid/*
+if [[ $(find /openvpn/pid -type f | wc -l) -ne 0 ]]; then
+rm -rf /openvpn/pid/*
 fi
 
 chown -R abc:abc /config/log
-chmod -R 755 /openvpn-pid
+chmod -R 755 /openvpn
