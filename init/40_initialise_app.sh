486680c5e2a26ee03aa25e4fb82a78bc6a4c9a0f
sparklyballs <sparklyballs@gmail.com>
2015-09-15 16:44:05 +0100

    swap to admin user for administation through webui

--- init/40_initialise_app.sh
+++ init/40_initialise_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $(find /config/etc/db -type f | wc -l) -eq 0 ]]; then
-printf 'DELETE\nyes\nyes\n1\n943\n9443\nyes\nyes\nno\nyes\nno\nabc\n\n' | /config/bin/ovpn-init
+printf 'DELETE\nyes\nyes\n1\n943\n9443\nyes\nyes\nno\nyes\nno\nadmin\n\n' | /config/bin/ovpn-init
 /etc/init.d/openvpnas stop
 chown -R abc:abc /config
 fi
