7e862e5207becdc0cda57d6f241324fb3f44b52c
sparklyballs <sparklyballs@gmail.com>
2015-09-18 01:32:31 +0100

    use variables for initialise options

--- init/40_initialise_app.sh
+++ init/40_initialise_app.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+NOASCONFIG="DELETE\n"
+ASCONFIG="yes\nyes\n1\n943\n9443\nyes\nyes\nno\nyes\nno\nadmin\n\n"
+
+if [ ! -f "/config/etc/as.conf" ]; then
+CONFINPUT=$ASCONFIG
+else
+CONFINPUT=$NOASCONFIG$ASCONFIG
+fi
+
 if [[ $(find /config/etc/db -type f | wc -l) -eq 0 ]]; then
-printf 'DELETE\nyes\nyes\n1\n943\n9443\nyes\nyes\nno\nyes\nno\nadmin\n\n' | /config/bin/ovpn-init
+printf "${CONFINPUT}" | /config/bin/ovpn-init
 /etc/init.d/openvpnas stop
 chown -R abc:abc /config
 fi
